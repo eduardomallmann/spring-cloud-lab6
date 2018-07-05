0a1ba762dd71af46bd3146467d9a9b297c579d1f
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-07-05 19:26:28 -0300

    Add exec1

---
+++ security-server/src/main/java/com/treinamento/voffice/securityserver/SecurityServerApplication.java
@@ -0,0 +1,12 @@
+package com.treinamento.voffice.securityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServerApplication.class, args);
+	}
+}
